1a7f80e367fb7e816c618485b67972329b0738b5
mikescor <mykytyn.mykhailo@gmail.com>
2018-03-14 08:37:06 +0200

    Add celery task to follow people

--- utils/twitterbot.py
+++ utils/twitterbot.py
@@ -1,33 +1,21 @@
-import csv
 import logging
-import os
 import time
-
-import click
 import tweepy
+
+from bot import settings
+from twitterbot.models import TargetTwitterAccounts
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-@click.command()
-@click.option('--filename', default='twitter_usernames.csv',
-              help='filename with usernames list of users you want to follow')
-@click.option('--limit', default=1000,
-              help='set limit amount of users you want to follow')
-def main(limit, filename):
-    with open(filename, 'r') as file:
-        list_users = []
-        reader = csv.reader(file)
-        for row in reader:
-            list_users.append(row[0])
-    list_users = list(set(list_users))
-    logger.info("Successfull reading of usernames file")
+def main(limit=1000):
+    list_users = TargetTwitterAccounts.objects.values_list('twitter_url', flat=True)
 
-    consumer_key = os.environ.get('CONSUMER_KEY')
-    consumer_secret = os.environ.get('CONSUMER_SECRET')
-    access_token = os.environ.get('ACCESS_TOKEN')
-    access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
+    consumer_key = settings.CONSUMER_KEY
+    consumer_secret = settings.CONSUMER_SECRET
+    access_token = settings.ACCESS_TOKEN
+    access_token_secret = settings.ACCESS_TOKEN_SECRET
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
@@ -41,36 +29,23 @@ def main(limit, filename):
     current_user = api.me().screen_name
     cached_users = cache_handler(api, current_user)
     counter = 0
-    try:
-        with open('followers_cache_{}.txt'.format(current_user), 'r') as f:
-            cached_local_users = [line.rstrip('\n') for line in f]
-        logger.debug("Successfull reading of local cache")
-    except:
-        cached_local_users = []
-        logger.debug("No local cache detected, new cache file will be created")
-    cached_users.extend(cached_local_users)
-    cached_users = list(set(cached_users))
-    try:
-        for user in list_users:
-            if user and user not in cached_users:
-                try:
-                    if api.get_user(user).followers_count > 300:
-                        logger.info("Follow %s", user)
-                        api.create_friendship(user)
-                except tweepy.error.TweepError:
-                    logger.exception('')
-                cached_users.append(user)
-                counter += 1
-                if counter >= limit:
-                    logger.info("The limit of %s followings is reached", limit)
-                    return
-            else:
-                logger.info("Skipped %s", user)
-    finally:
-        logger.info('Writiind local cache into"followers_cache_%s.txt"',
-                    current_user)
-        with open('followers_cache_{}.txt'.format(current_user), 'w') as f:
-            f.writelines(["%s\n" % item for item in cached_users])
+
+    for user in list_users:
+        if user and user not in cached_users:
+            user = user.split('/')[3]
+            try:
+                if api.get_user(user).followers_count > 300:
+                    logger.info("Follow %s", user)
+                    api.create_friendship(user)
+            except tweepy.error.TweepError:
+                logger.exception('')
+            cached_users.append(user)
+            counter += 1
+            if counter >= limit:
+                logger.info("The limit of %s followings is reached", limit)
+                return
+        else:
+            logger.info("Skipped %s", user)
 
 
 def cache_handler(api, current_user):
@@ -95,7 +70,3 @@ def split_list(alist, wanted_parts=1):
     length = len(alist)
     return [alist[i * length // wanted_parts: (i + 1) * length // wanted_parts]
             for i in range(wanted_parts)]
-
-
-if __name__ == "__main__":
-    main()
