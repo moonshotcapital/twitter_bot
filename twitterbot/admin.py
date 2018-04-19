50228568d4d4a625ef8a39565ce90e3ad16c76be
Klochenok Roman <r.klochenok@gmail.com>
2018-04-19 11:58:39 +0300

    Change models for handling twitter users

--- twitterbot/admin.py
+++ twitterbot/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import TwitterUser, Followers, TargetTwitterAccounts, BlackList
+from .models import TwitterUser, TargetTwitterAccount, BlackList
 
 admin.site.register(TwitterUser)
-admin.site.register(Followers)
-admin.site.register(TargetTwitterAccounts)
+admin.site.register(TargetTwitterAccount)
 admin.site.register(BlackList)
--- twitterbot/models.py
+++ twitterbot/models.py
@@ -2,40 +2,37 @@ from django.db import models
 
 
 class TwitterUser(models.Model):
+
+    FRIEND = 1
+    FOLLOWER = 2
+
+    USER_TYPE_CHOICES = ((FRIEND, 'Friend'), (FOLLOWER, 'Follower'))
+
     user_id = models.PositiveIntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     screen_name = models.CharField(max_length=50)
-    location = models.CharField(max_length=200, null=True)
-    url = models.URLField(null=True)
-    description = models.CharField(max_length=200, null=True)
-    created_at = models.DateTimeField()
+    followers_count = models.PositiveIntegerField(null=True)
+    user_type = models.IntegerField(choices=USER_TYPE_CHOICES, null=True)
+
+    location = models.CharField(max_length=200, null=True, blank=True)
+    url = models.URLField(null=True, blank=True)
+    description = models.CharField(max_length=200, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'twitter_users'
 
     def __str__(self):
-        return self.name
+        return self.screen_name
 
 
-class Followers(models.Model):
-    user_id1 = models.ForeignKey(TwitterUser, on_delete=models.CASCADE,
-                                 related_name='publisher')
-    user_id2 = models.ForeignKey(TwitterUser, on_delete=models.CASCADE,
-                                 related_name='subscriber')
+class TargetTwitterAccount(models.Model):
+    user_id = models.PositiveIntegerField(unique=True)
+    name = models.CharField(max_length=100)
+    screen_name = models.CharField(max_length=50)
+    followers_count = models.PositiveIntegerField(null=True)
 
-    class Meta:
-        db_table = 'followers'
-
-    def __str__(self):
-        return '{} follows {}'.format(self.user_id2.name, self.user_id1.name)
-
-
-class TargetTwitterAccounts(models.Model):
-    first_name = models.CharField(max_length=100, db_index=True)
-    last_name = models.CharField(max_length=100, db_index=True)
-    twitter_url = models.URLField(null=True)
-    title = models.CharField(max_length=200, blank=True, null=True,
-                             db_index=True)
     location_city = models.CharField(max_length=100, blank=True, null=True)
     location_region = models.CharField(max_length=100, blank=True, null=True)
     location_country_code = models.CharField(max_length=100, blank=True,
@@ -45,10 +42,6 @@ class TargetTwitterAccounts(models.Model):
 
     class Meta:
         db_table = 'target_twitter_accounts'
-
-    @property
-    def full_name(self):
-        return '{} {}'.format(self.first_name, self.last_name)
 
     @property
     def location(self):
