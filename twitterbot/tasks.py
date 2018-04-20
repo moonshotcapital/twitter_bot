bfdd62cd1d0ae6ae0859673aaa505e95655d3492
Klochenok Roman <r.klochenok@gmail.com>
2018-04-20 17:12:54 +0300

    Refactor updating db followers list

--- twitterbot/tasks.py
+++ twitterbot/tasks.py
@@ -1,7 +1,7 @@
 from celery import task
 from celery.utils.log import get_task_logger
 from utils.twitterbot import main
-from utils.get_friends_list import update_followers_list
+from utils.get_friends_list import update_twitter_followers_list
 
 logger = get_task_logger(__name__)
 
@@ -15,5 +15,5 @@ def follow_people():
 @task
 def update_followers_list_task():
     logger.info('Started updating followers list!')
-    update_followers_list()
+    update_twitter_followers_list()
     logger.info('Finished updating followers list!')
--- utils/get_friends_list.py
+++ utils/get_friends_list.py
@@ -14,7 +14,7 @@ ACCESS_TOKEN = settings.ACCESS_TOKEN
 ACCESS_TOKEN_SECRET = settings.ACCESS_TOKEN_SECRET
 
 
-def update_followers_list():
+def update_twitter_followers_list():
 
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
@@ -23,15 +23,53 @@ def update_followers_list():
     friends_list = api.friends()
     followers_list = api.followers()
 
+    # sync db if someone unsubscribed from us or etc.
+    update_db_followers_list_due_to_non_automatic_changes(friends_list,
+                                                          followers_list)
+    # add new friends and followers to db after automation marketing
+    update_db_friends_list(friends_list)
+    update_db_followers_list(followers_list)
+
+
+def update_db_followers_list_due_to_non_automatic_changes(friends_list,
+                                                          followers_list):
+    db_followers = TwitterFollower.objects.filter(
+        user_type=TwitterFollower.FOLLOWER
+    ).values_list('user_id', flat=True)
+
+    for db_follower in db_followers:
+        tw_foolowers = [user.id for user in followers_list]
+        if db_follower not in tw_foolowers:
+            TwitterFollower.objects.filter(
+                user_id=db_follower,
+                user_type=TwitterFollower.FOLLOWER
+            ).delete()
+
+    db_friends = TwitterFollower.objects.filter(
+        user_type=TwitterFollower.FRIEND
+    ).values_list('user_id', flat=True)
+
+    for db_friend in db_friends:
+        tw_friends = [user.id for user in friends_list]
+        if db_friend not in tw_friends:
+            TwitterFollower.objects.filter(
+                user_id=db_friend,
+                user_type=TwitterFollower.FRIEND
+            ).delete()
+
+
+def update_db_friends_list(friends_list):
     for friend in friends_list:
         user_type = TwitterFollower.FRIEND
         friend_exist = TwitterFollower.objects.filter(
             user_id=friend.id,
             user_type=user_type
         ).exists()
+
         exist_in_black_list = BlackList.objects.filter(
             user_id=friend.id
         ).exists()
+
         if not friend_exist and not exist_in_black_list:
             friends_info = {
                 'user_id': friend.id,
@@ -43,10 +81,12 @@ def update_followers_list():
             }
             TwitterFollower.objects.create(**friends_info)
         elif friend_exist and exist_in_black_list:
-            TwitterFollower.objects.delete(user_id=friend.id)
+            TwitterFollower.objects.filter(user_id=friend.id).delete()
         else:
             continue
 
+
+def update_db_followers_list(followers_list):
     for follower in followers_list:
         user_type = TwitterFollower.FOLLOWER
         follower_exist = TwitterFollower.objects.filter(
@@ -67,6 +107,6 @@ def update_followers_list():
             }
             TwitterFollower.objects.create(**follower_info)
         elif follower_exist and exist_in_black_list:
-            TwitterFollower.objects.delete(user_id=follower.id)
+            TwitterFollower.objects.filter(user_id=follower.id).delete()
         else:
             continue
