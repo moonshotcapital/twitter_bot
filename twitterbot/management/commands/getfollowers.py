dd858b20e4e3a6ffeda61ee35f66e9e44dd8323b
mikescor <mykytyn.mykhailo@gmail.com>
2018-04-23 20:16:04 +0300

    Add pagination for fetching followers

--- twitterbot/management/commands/getfollowers.py
+++ twitterbot/management/commands/getfollowers.py
@@ -1,9 +1,13 @@
+import logging
 from django.core.management.base import BaseCommand
 from django.conf import settings
 
 import tweepy
 
 from twitterbot.models import TargetTwitterAccount
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 CONSUMER_KEY = settings.CONSUMER_KEY
 CONSUMER_SECRET = settings.CONSUMER_SECRET
@@ -38,23 +42,27 @@ class Command(BaseCommand):
             self.stdout.write('Loading followers of {}'.format(user))
 
             try:
-                followers_list = api.get_user(user).followers()
-                for follower in followers_list:
-                    follower_exist = TargetTwitterAccount.objects.filter(
-                        user_id=follower.id
-                    ).exists()
+                for page in tweepy.Cursor(api.followers_ids,
+                                          screen_name=user).pages():
+                    for follower_id in page:
+                        follower = api.get_user(id=follower_id)
+                        follower_exist = TargetTwitterAccount.objects.filter(
+                            user_id=follower.id
+                        ).exists()
 
-                    if not follower_exist:
-                        follower_info = {
-                            'user_id': follower.id,
-                            'name': follower.name,
-                            'screen_name': follower.screen_name,
-                            'followers_count': follower.followers_count,
-                            'location': follower.location
-                        }
-                        TargetTwitterAccount.objects.create(**follower_info)
-                    else:
-                        continue
+                        if not follower_exist:
+                            follower_info = {
+                                'user_id': follower.id,
+                                'name': follower.name,
+                                'screen_name': follower.screen_name,
+                                'followers_count': follower.followers_count,
+                                'location': follower.location
+                            }
+                            TargetTwitterAccount.objects.create(**follower_info)
+                            logger.info("Save %s", follower.name)
+                        else:
+                            logger.info("Skipped %s", follower.name)
+                            continue
 
                 self.stdout.write(
                     self.style.SUCCESS('Successfully loaded followers of {}'
@@ -68,23 +76,27 @@ class Command(BaseCommand):
             self.stdout.write('Loading friends of {}'.format(user))
 
             try:
-                friends_list = api.get_user(user).friends()
-                for friend in friends_list:
-                    friend_exist = TargetTwitterAccount.objects.filter(
-                        user_id=friend.id
-                    ).exists()
+                for page in tweepy.Cursor(api.friends_ids,
+                                          screen_name=user).pages():
+                    for friend_id in page:
+                        friend = api.get_user(id=friend_id)
+                        friend_exist = TargetTwitterAccount.objects.filter(
+                            user_id=friend.id
+                        ).exists()
 
-                    if not friend_exist:
-                        friend_info = {
-                            'user_id': friend.id,
-                            'name': friend.name,
-                            'screen_name': friend.screen_name,
-                            'followers_count': friend.followers_count,
-                            'location': friend.location
-                        }
-                        TargetTwitterAccount.objects.create(**friend_info)
-                    else:
-                        continue
+                        if not friend_exist:
+                            friend_info = {
+                                'user_id': friend.id,
+                                'name': friend.name,
+                                'screen_name': friend.screen_name,
+                                'followers_count': friend.followers_count,
+                                'location': friend.location
+                            }
+                            TargetTwitterAccount.objects.create(**friend_info)
+                            logger.info("Save %s", friend.name)
+                        else:
+                            logger.info("Skipped %s", friend.name)
+                            continue
 
                 self.stdout.write(
                     self.style.SUCCESS('Successfully loaded friends of {}'
