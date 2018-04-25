b238cda8d84f0f1174cc4d6d9e1d2c0d890e4a4c
Klochenok Roman <r.klochenok@gmail.com>
2018-04-25 12:33:08 +0300

    Add commit search by tag

--- utils/retweet_tag.py
+++ utils/retweet_tag.py
@@ -1,30 +1,57 @@
 import logging
-import os
+from django.conf import settings
 
 import tweepy
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+CONSUMER_KEY = settings.CONSUMER_KEY
+CONSUMER_SECRET = settings.CONSUMER_SECRET
+ACCESS_TOKEN = settings.ACCESS_TOKEN
+ACCESS_TOKEN_SECRET = settings.ACCESS_TOKEN_SECRET
 
-def main():
-    consumer_key = os.environ.get('CONSUMER_KEY')
-    consumer_secret = os.environ.get('CONSUMER_SECRET')
-    access_token = os.environ.get('ACCESS_TOKEN')
-    access_token_secret = os.environ.get('ACCESS_TOKEN_SECRET')
 
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(
-        auth, wait_on_rate_limit=True,
-        wait_on_rate_limit_notify=True,
-        retry_count=10, retry_delay=5,
-        retry_errors=None
-    )
+HASHTAG_QUERY = [
+    '#ios',
+    '#iosdev',
+    '#itunesconnect',
+    '#appstore',
+    '#googleplay',
+    '#mobileapps',
+    '#mobilegames',
+    '#appdev',
+    '#appdevelopement',
+    '#appbusiness',
+    '#gamedev',
+    '#ASO',
+    '#appstoreoptimization',
+    '#mobile growth',
+    '#appmarketing',
+    '#mobilemarketing',
+    '#digitalmarketing',
+    '#appanalytics',
+    '#mobileanalytics',
+    '#mobile',
+    '#useracquisition',
+    '#ROI',
+    '#entrepreneur',
+    '#growthhacking',
+    '#indiedev'
+]
 
-    query = 'python programming'
-    max_tweets = 10000
-    searched_tweets = [status for status in tweepy.Cursor(
+
+def retweet_by_tag():
+
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True)
+
+    # TODO: need to define how many and which tweets need to be retweeted
+    max_tweets = 10
+    query = HASHTAG_QUERY[0]
+    searched_tweets = [tweet for tweet in tweepy.Cursor(
         api.search, q=query, lang='en').items(max_tweets)]
 
     for tweet in searched_tweets:
@@ -32,7 +59,3 @@ def main():
                 and tweet.retweet_count > 50 and tweet.favorite_count > 50:
             api.retweet(tweet.id)
             logger.info('Retweet %s', tweet.id)
-
-
-if __name__ == "__main__":
-    main()
