55ecc5a5bb1ac3a7a65e68784aa1b8539b361490
Klochenok Roman <r.klochenok@gmail.com>
2018-07-03 17:29:49 +0300

    Add manage command for creating dataset in csv

---
+++ twitterbot/management/commands/twitter_followers.py
@@ -0,0 +1,32 @@
+import csv
+import logging
+from django.core.management.base import BaseCommand
+
+from twitterbot.models import AccountOwner
+from utils.common import connect_to_twitter_api
+from utils.get_followers_and_friends import get_followers
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+
+    help = 'Loads followers for creating dataset in csv'
+
+    def add_arguments(self, parser):
+        parser.add_argument('username', nargs='+', type=str)
+
+    def handle(self, *args, **options):
+        self.stdout.write('Start process')
+        tw_user = options['username'][0]
+        account = AccountOwner.objects.filter(is_active=True).first()
+        api = connect_to_twitter_api(account)
+        tw_user = api.get_user(tw_user)
+        followers = get_followers(tw_user)
+        folowers_with_a = [['@' + i.screen_name] for i in followers]
+
+        file_name = "{}_followers.csv".format(tw_user.screen_name)
+        with open("/home/ubuntu/{}".format(file_name), 'w') as file:
+            wr = csv.writer(file)
+            wr.writerows(folowers_with_a)
