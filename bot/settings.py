a40f86e2f92a7aa3734733b39f318357971d169b
Klochenok Roman <r.klochenok@gmail.com>
2018-07-11 16:09:25 +0300

    Change followers limit for goformoonshot

--- bot/settings.py
+++ bot/settings.py
@@ -196,7 +196,7 @@ TWITTER_ACCOUNT_SETTINGS = {
         'unfollow': ['utils.twitterbot.make_unfollow_for_current_account'],
         'follow': ['utils.twitterbot.make_follow_for_current_account',
                    'utils.twitterbot.follow_all_own_followers'],
-        'followers_limit': 50,
+        'followers_limit': 100,
         'retweet': True
     },
 }
